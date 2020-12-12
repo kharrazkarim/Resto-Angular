5acd28e9f11b6e641e768a4a30622f5c0c9c000b
kh_karim <kharrazkarim1@gmail.com>
2020-12-12 15:17:10 +0100

    Reactive Forms Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,6 +29,9 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {FormsModule} from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select'
+import {MatSlideToggleModule} from '@angular/material/slide-toggle'
 import { from } from 'rxjs';
 
 
@@ -58,7 +61,11 @@ import { from } from 'rxjs';
     MatFormFieldModule,
     MatInputModule,
     MatCheckboxModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    MatSlideToggleModule,
+    MatSelectModule
+
   ],
   providers: [DishService,PromotionService,LeaderService],
   entryComponents:[LoginComponent],
